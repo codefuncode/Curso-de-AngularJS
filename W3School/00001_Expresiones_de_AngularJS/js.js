63a92448c688cd13cac4237bd744d0a0e878cbf9
Carlos Aleman <caleman9791@gmail.com>
2022-04-10 15:03:28 -0400

    Expresiones con AngularJS

---
+++ W3School/00001_Expresiones_de_AngularJS/js.js
@@ -0,0 +1,58 @@
+var app = angular.module("App", []);
+
+// ////////////////////////////////////////////////
+app.controller("Ejemplo1", function($scope) {
+  $scope.numeroEjemplo = "1";
+  $scope.firstName = "John";
+  $scope.lastName = "Doe";
+});
+
+// ////////////////////////////////////////////////
+app.controller("Ejemplo2", function($scope) {
+  $scope.numeroEjemplo = "2";
+  $scope.firstName = "Carlos";
+  $scope.lastName = "Alohan";
+});
+
+// ////////////////////////////////////////////////
+app.controller("Ejemplo3", function($scope) {
+  $scope.numeroEjemplo = "3";
+  $scope.color = "lightblue";
+
+});
+// ////////////////////////////////////////////////
+app.controller("Ejemplo4", function($scope) {
+  $scope.numeroEjemplo = "4";
+  $scope.quantity = "5";
+  $scope.cost = "2";
+
+});
+// ////////////////////////////////////////////////
+app.controller("Ejemplo5", function($scope) {
+  $scope.numeroEjemplo = "5";
+  $scope.quantity = "5";
+  $scope.cost = "2";
+
+});
+// ////////////////////////////////////////////////
+app.controller("Ejemplo6", function($scope) {
+  $scope.numeroEjemplo = "6";
+  $scope.firstName = "Carlos";
+  $scope.lastName = "Aleman";
+
+});
+// ////////////////////////////////////////////////
+app.controller("Ejemplo7", function($scope) {
+  $scope.numeroEjemplo = "7";
+  $scope.persona = {
+    firstName: "Carlos",
+    lastName: "Aleman"
+  };
+
+});
+// ////////////////////////////////////////////////
+app.controller("Ejemplo8", function($scope) {
+  $scope.numeroEjemplo = "8";
+  $scope.points = [1, 2, 3, 4, 5, 10, 15, 20];
+
+});
