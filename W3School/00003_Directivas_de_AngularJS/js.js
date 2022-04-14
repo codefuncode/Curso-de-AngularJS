bd1196cb39986046179d7deff1551202f3188392
Carlos Aleman <caleman9791@gmail.com>
2022-04-13 20:18:33 -0400

    Módulo 3 sobre directivas.

--- W3School/00003_Directivas_de_AngularJS/js.js
+++ W3School/00003_Directivas_de_AngularJS/js.js
@@ -5,3 +5,16 @@ app.controller('Nombre_Controlador', function($scope) {
   $scope.name = "John Doe";
 
 });
+
+app.controller("Ejemplo1", function($scope) {
+  $scope.firstName = "John";
+  $scope.lastName = "Doe";
+});
+
+app.controller('Ejemplo2', function($scope) {
+  $scope.count = 0;
+  //  Definición de funciones en AngularjS
+  $scope.myFunction = function() {
+    $scope.count++;
+  }
+});
